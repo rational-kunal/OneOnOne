1ccd77cb9deb07e274c3050d5e6db0826712f6bc
kunal <kunalgk1999@gmail.com>
2020-04-27 20:17:45 +0530

    Updates

---
+++ precache-manifest.8f1e843e94d2ca9fa1ce44105041866b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b95d13cf4c584c7eb3d4a39fbdfb8ae",
+    "url": "/OneOnOne/index.html"
+  },
+  {
+    "revision": "347cc508d64dd5f481db",
+    "url": "/OneOnOne/static/css/main.c0c92423.chunk.css"
+  },
+  {
+    "revision": "5c43f8ed985c0a0b7bf4",
+    "url": "/OneOnOne/static/js/2.fd6c108c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/OneOnOne/static/js/2.fd6c108c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "347cc508d64dd5f481db",
+    "url": "/OneOnOne/static/js/main.28d3cd8a.chunk.js"
+  },
+  {
+    "revision": "a50bdc07b05837343c60",
+    "url": "/OneOnOne/static/js/runtime-main.034b757f.js"
+  }
+]);
